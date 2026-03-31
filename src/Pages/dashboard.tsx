bb43e632dda59ce37c65c036ca323a2df49dc0a3
pejuameenat <pejuameenat@gmail.com>
2026-03-31 22:21:21 +0100

    conditionally show how many med taken in a day if theres a medication for that day

--- src/Pages/dashboard.tsx
+++ src/Pages/dashboard.tsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
 import { useMemo } from "react";
-import MedDetails from '../modules/dashboard/details'
-import { auth } from '../Firebase/config'
+import MedDetails from "../modules/dashboard/details";
+import { auth } from "../Firebase/config";
 import { fetchMedications } from "../lib/utilqueries.ts/medicationquery";
 import { useQuery } from "@tanstack/react-query";
-import { Loader2,Package } from "lucide-react";
+import { Loader2, Package } from "lucide-react";
 import { useAuth } from "../Firebase/auth-context";
 
 const Dashboard = () => {
-  const {userId, loading } = useAuth()
-  
+  const { userId, loading } = useAuth();
+
   const {
     data: medications = [],
     isLoading,
@@ -17,30 +17,40 @@ const Dashboard = () => {
   } = useQuery({
     queryKey: ["medications", userId],
     queryFn: () => fetchMedications(userId!),
-    enabled: !!userId||!loading,
+    enabled: !!userId || !loading,
   });
 
-  const todaysMedications = useMemo(() => 
-   medications?.filter((med) => {
-      return new Date(med?.medStart ?? '').toDateString() === new Date().toDateString() || med?.createdAt?.toDate().toDateString() === new Date().toDateString()
-    })
-    , [medications])
-  
-  const takenMedications = useMemo(() => 
-   todaysMedications?.filter((med) => {
-      return  med?.isTaken === true
-    })
-    , [todaysMedications])
-  
-   const notTakenMedications = useMemo(() => 
-   todaysMedications?.filter((med) => {
-      return  med?.isTaken === false
-    })
-     , [todaysMedications])
-  
+  const todaysMedications = useMemo(
+    () =>
+      medications?.filter((med) => {
+        return (
+          new Date(med?.medStart ?? "").toDateString() ===
+            new Date().toDateString() ||
+          med?.createdAt?.toDate().toDateString() === new Date().toDateString()
+        );
+      }),
+    [medications],
+  );
+
+  const takenMedications = useMemo(
+    () =>
+      todaysMedications?.filter((med) => {
+        return med?.isTaken === true;
+      }),
+    [todaysMedications],
+  );
+
+  const notTakenMedications = useMemo(
+    () =>
+      todaysMedications?.filter((med) => {
+        return med?.isTaken === false;
+      }),
+    [todaysMedications],
+  );
+
   //use interval and updatedAt to determine next medication and time until next medication
-  
- if (isLoading) {
+
+  if (isLoading) {
     return (
       <div className="flex justify-center items-center h-screen">
         <Loader2 size={40} className="animate-spin" />
@@ -60,7 +70,8 @@ const Dashboard = () => {
       <div className="pb-8 ">
         <h1 className="text-xl lg:text-3xl font-semibold">Dashboard</h1>
         <span className="text[12px] text-[#141414]">
-         Welcome <strong>{auth.currentUser?.displayName}</strong> , your medication overview for today
+          Welcome <strong>{auth.currentUser?.displayName}</strong> , your
+          medication overview for today
         </span>
       </div>
       <div className="lg:grid lg:grid-cols-4 gap-4">
@@ -70,10 +81,18 @@ const Dashboard = () => {
               <strong className="block text-[#141414] font-medium">
                 Today's Medication
               </strong>
-              <span className="text-[12px]">{takenMedications?.length } of {todaysMedications?.length} taken </span>
-              <span className="text-[12px]">
-                • Next: {notTakenMedications[0]?.medName} at  {notTakenMedications[0]?.medInterval}
-              </span>
+              {todaysMedications?.length > 0 && (
+                <>
+                  <span className="text-[12px]">
+                    {takenMedications?.length} of {todaysMedications?.length}{" "}
+                    taken{" "}
+                  </span>
+                  <span className="text-[12px]">
+                    • Next: {notTakenMedications[0]?.medName} at{" "}
+                    {notTakenMedications[0]?.medInterval}
+                  </span>
+                </>
+              )}
             </div>
             <Link
               to="/medications"
@@ -83,21 +102,20 @@ const Dashboard = () => {
             </Link>
           </div>
 
-            {todaysMedications.length === 0 ? (
-                    <div className="flex  flex-col items-center justify-center h-screen text-gray-600">
-                      <Package size={100} />
-                      <span>You don't have any medications for today.</span>
-                    </div>
-                  ) : (
-                    <article className="pt-4 flex flex-col  gap-5">
-                      {/* items */}
-                      {todaysMedications?.map((med) => {
-                        return <MedDetails med={ med} key={med?.id} />
-                      })}
-                    </article>
-                  )}
-          <div>
-          </div>
+          {todaysMedications.length === 0 ? (
+            <div className="flex  flex-col items-center justify-center h-screen text-gray-600">
+              <Package size={100} />
+              <span>You don't have any medications for today.</span>
+            </div>
+          ) : (
+            <article className="pt-4 flex flex-col  gap-5">
+              {/* items */}
+              {todaysMedications?.map((med) => {
+                return <MedDetails med={med} key={med?.id} />;
+              })}
+            </article>
+          )}
+          <div></div>
         </article>
         {/* <div className="bg-white rounded-md p-4 border border-[#bdbdbd] text-sm mt-4 lg:mt-0">
           <span className="font-semibold  ">Todays's Adherence</span>
