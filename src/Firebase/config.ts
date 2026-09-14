8717b645ae50477802d2f9081d0aef9abdd2c155
pejuameenat <pejuameenat@gmail.com>
2026-09-15 00:04:55 +0100

    resolved build error

--- src/Firebase/config.ts
+++ src/Firebase/config.ts
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
-import { getMessaging , onMessage} from "firebase/messaging";
+import { getMessaging} from "firebase/messaging";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
